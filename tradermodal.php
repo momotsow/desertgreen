9e3bc15db8a58ed6835738eec4db812ee7f328a3
momotsow <kgomotsomkansi@gmail.com>
2022-11-10 17:34:06 +0200

    files

--- tradermodal.php
+++ tradermodal.php
@@ -1,45 +1,31 @@
+<!-- Contact Form in PHP -->
 <?php
+  $farmer = $_POST['farmers'];
+  $name = strip_tags(htmlspecialchars($_POST['name']));
+  $email_address = strip_tags(htmlspecialchars($_POST['email']));
+  $phone = strip_tags(htmlspecialchars($_POST['phone']));
+  $location = strip_tags(htmlspecialchars($_POST['location']));
+  $farmname = strip_tags(htmlspecialchars($_POST['farmname']));
+  $plotsize = strip_tags(htmlspecialchars($_POST['plotsize']));
+  $farmowner = strip_tags(htmlspecialchars($_POST['farmowner']));
+  $additional = strip_tags(htmlspecialchars($_POST['additional']));
+  $selectedCrops = implode(' | ', $_POST['crops']);
 
-header('Access-Control-Allow-Origin: *');
-
-// Check for empty fields
-if(empty($_POST['farmers'])      ||
-   empty($_POST['name'])    ||
-   empty($_POST['email'])    ||
-   empty($_POST['phone'])     ||
-   empty($_POST['location'])  ||
-   empty($_POST['farmname'])  ||
-   empty($_POST['plotsize'])  ||
-   empty($_POST['farmowner'])  ||
-   empty($_POST['additional'])  ||
-   count($_POST['crops[]']) < 0  ||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-   echo "No arguments Provided!";
-   return false;
-   }
-
-$farmer = $_POST['farmers'];
-$name = strip_tags(htmlspecialchars($_POST['name']));
-$email_address = strip_tags(htmlspecialchars($_POST['email']));
-$phone = strip_tags(htmlspecialchars($_POST['phone']));
-$location = strip_tags(htmlspecialchars($_POST['location']));
-$farmname = strip_tags(htmlspecialchars($_POST['farmname']));
-$plotsize = strip_tags(htmlspecialchars($_POST['plotsize']));
-$farmowner = strip_tags(htmlspecialchars($_POST['farmowner']));
-$additional = strip_tags(htmlspecialchars($_POST['additional']));
-
-$selectedCrops = implode(' | ', $_POST['crops']);
-
-   
-// Create the email and send the message
-$to = 'kmkansi@paperinstrument.com'; 
-$email_subject = "Website's Expression of Interest Form:  $farmer";
-$email_body = "You have received a new message from your Website's Expression of Interest Form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nLocation: $location\n\nFarm Name: $farmname\n\nPlot Size: $plotsize\n\nFarm Owner's Name: $farmowner\n\nAdditional Message: $additional\n\n\n\nSelected Crops: $selectedCrops\n\n";
-
-$headers = "From: kmkansi@paperinstrument\n";
-
-$headers .= "Reply-To: $email_address";   
-mail($to,$email_subject,$email_body,$headers);
-return true;         
+  if(!empty($email_address) && !empty($message)){
+    if(filter_var($email_address, FILTER_VALIDATE_EMAIL)){
+      $receiver = "kmkansi@paperinstrument.com"; //enter that email address where you want to receive all messages
+      $subject = "From: $name <$email_address>";
+      $body = "You have received a new message from your Website's Expression of Interest Form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nLocation: $location\n\nFarm Name: $farmname\n\nPlot Size: $plotsize\n\nFarm Owner's Name: $farmowner\n\nAdditional Message: $additional\n\n\n\nSelected Crops: $selectedCrops\n\n";
+      $sender = "From: $email_address";
+      if(mail($receiver, $subject, $body, $sender)){
+         echo "Your message has been sent";
+      }else{
+         echo "Sorry, failed to send your message!";
+      }
+    }else{
+      echo "Enter a valid email address!";
+    }
+  }else{
+    echo "Email and message field is required!";
+  }
 ?>
