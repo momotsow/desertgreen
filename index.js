a64f885a59aaadd7f6028f04890d3601e1777ca6
momotsow <kgomotsomkansi@gmail.com>
2023-01-29 14:07:34 +0200

    changes

--- index.js
+++ index.js
@@ -61,3 +61,22 @@ function showSlid(n) {
   slides[slideIndx-1].style.display = "block";  
   dots[slideIndx-1].className += " active";
 }
+
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
